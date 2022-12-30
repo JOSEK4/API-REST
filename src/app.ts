b53e28b4d120e14a3d05a0d1d2c2865267a2d285
Jose <jortiz6969@misena.edu.co>
2022-12-30 10:26:03 -0500

    agregando la conexion a la db

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { router } from "./routes";
+import db from "./config/mongo";
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(router);
+db().then(() => console.log("conectado a la base de datos"));
 app.listen(PORT, () => console.log(`listo por el puerto ${PORT}`));
